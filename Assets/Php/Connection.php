f68e177189e3a5d5f641d7240a75c3b9b8989161
ShaomyCode <johnsamueljamandron@gmail.com>
2024-08-23 00:53:37 +0800

    update

--- Assets/Php/Connection.php
+++ Assets/Php/Connection.php
@@ -9,7 +9,10 @@
 	$conn = new mysqli($servername,$username,$password,$dbname);
 
 	if($conn){
-
+		echo "string";
+		header('Location: ../../index.html');
+		sleep(15);
+		exit();
 	}else{
 		die(mysqli_error($conn));
 	}
